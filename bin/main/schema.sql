51b837eb2564b158df6de96a646e4835a5ed5e3a
JY8752 <j.y.8752@gmail.com>
2020-05-29 08:04:53 +0900

    m_userテーブル、データ作成

--- bin/main/schema.sql
+++ bin/main/schema.sql
@@ -1,7 +1,18 @@
 drop table if exists employee;
+drop table if exists m_user;
 
 create table employee(
   employee_id int primary key not null auto_increment,
   employee_name varchar(50),
   age int
+);
+
+create table m_user(
+    user_id varchar(50) primary key,
+    password varchar(100),
+    user_name varchar(50),
+    birtyday date,
+    age int,
+    marrige boolean,
+    role varchar(50)
 );
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,21 @@
 insert into employee(employee_id, employee_name, age)
 values(1, '山田太郎', 30);
+
+insert into m_user(
+    user_id,
+    password,
+    user_name,
+    birthday,
+    age,
+    marrige,
+    role
+)
+values(
+    'yamada@example.com',
+    'password',
+    '山田太郎',
+    '1990-01-01',
+    28,
+    false,
+    'ROLE_ADMIN'
+);
